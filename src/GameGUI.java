84bb624d8af150d9b0c7d03a7222b35cfa36fee3
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-11-30 15:50:31 +0100

    lagt till fr. felande, skrotade projektet alla paneler, button m.m.

---
+++ src/GameGUI.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+/**
+ * Created by: Ulf Nyberg
+ * Date: 2020-11-30
+ * Time: 15:49
+ * Project: Four-in-a-row
+ * Copyright: MIT
+ */
+public class GameGUI extends JFrame {
+
+    JFrame frame = new JFrame();
+
+    JPanel gameGrid = new JPanel();
+
+    public JLabel scoreLabel = new JLabel();
+    public JLabel infoLabel = new JLabel();
+    public JLabel p1ScoreCounter = new JLabel();
+    public JLabel p2ScoreCounter = new JLabel();
+
+    public JButton showRulesButton = new JButton();
+    public JButton p1ColorButtongroup = new JButton();
+    public JButton p2ColorButtongroup = new JButton();
+
+    public JRadioButton p1Color1 = new JRadioButton();
+    public JRadioButton p1Color2 = new JRadioButton();
+    public JRadioButton p1Color3 = new JRadioButton();
+    public JRadioButton p1Color4 = new JRadioButton();
+    public JRadioButton p2Color1 = new JRadioButton();
+    public JRadioButton p2Color2 = new JRadioButton();
+    public JRadioButton p2Color3 = new JRadioButton();
+    public JRadioButton p2Color4 = new JRadioButton();
+
+
+    public GameGUI (){
+
+
+
+
+
+
+
+    }
+
+    public void refreshGameGrid (){
+
+    }
+
+}
