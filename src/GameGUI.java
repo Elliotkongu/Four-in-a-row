d32541dfc9741f10ee5dfca7e29fa87d758725cb
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2020-12-01 09:46:08 +0100

    Layouts skapade, knappar adderade

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by: Ulf Nyberg
@@ -9,38 +10,72 @@ import javax.swing.*;
  */
 public class GameGUI extends JFrame {
 
-    JFrame frame = new JFrame();
-
-    JPanel gameGrid = new JPanel();
+    JPanel southPanel = new JPanel();
+    JPanel gridPanel = new JPanel();
+    JPanel southWestPanel = new JPanel();
+    JPanel southEastPanel = new JPanel();
+    JPanel southCenterPanel = new JPanel();
 
     public JLabel scoreLabel = new JLabel();
     public JLabel infoLabel = new JLabel();
     public JLabel p1ScoreCounter = new JLabel();
     public JLabel p2ScoreCounter = new JLabel();
 
-    public JButton showRulesButton = new JButton();
+    public JButton showRulesButton = new JButton("Spelregler");
+    public JButton undoButton = new JButton("Ångra");
 
     public ButtonGroup p1ColorButtongroup = new ButtonGroup();
     public ButtonGroup p2ColorButtongroup = new ButtonGroup();
 
-    public JRadioButton p1Color1 = new JRadioButton();
-    public JRadioButton p1Color2 = new JRadioButton();
-    public JRadioButton p1Color3 = new JRadioButton();
-    public JRadioButton p1Color4 = new JRadioButton();
-    public JRadioButton p2Color1 = new JRadioButton();
-    public JRadioButton p2Color2 = new JRadioButton();
-    public JRadioButton p2Color3 = new JRadioButton();
-    public JRadioButton p2Color4 = new JRadioButton();
+    public JRadioButton p1Color1 = new JRadioButton("Rött");
+    public JRadioButton p1Color2 = new JRadioButton("Blå");
+    public JRadioButton p1Color3 = new JRadioButton("Lila");
+    public JRadioButton p2Color1 = new JRadioButton("Gult");
+    public JRadioButton p2Color2 = new JRadioButton("Rosa");
+    public JRadioButton p2Color3 = new JRadioButton("Grönt");
 
 
     public GameGUI (){
 
+        setLayout(new BorderLayout());
+
+        p1ColorButtongroup.add(p1Color1);
+        p1ColorButtongroup.add(p1Color2);
+        p1ColorButtongroup.add(p1Color3);
+        p2ColorButtongroup.add(p2Color1);
+        p2ColorButtongroup.add(p2Color2);
+        p2ColorButtongroup.add(p2Color3);
+        add(gridPanel, BorderLayout.CENTER);
+        gridPanel.setLayout(new GridLayout(6,7));
+        add(southPanel, BorderLayout.SOUTH);
+        southPanel.setLayout(new BorderLayout());
+        southPanel.add(southWestPanel, BorderLayout.WEST);
+        southPanel.add(southEastPanel, BorderLayout.EAST);
+        southPanel.add(southCenterPanel, BorderLayout.CENTER);
+        southWestPanel.add(p1Color1);
+        southWestPanel.add(p1Color2);
+        southWestPanel.add(p1Color3);
+        southEastPanel.add(p2Color1);
+        southEastPanel.add(p2Color2);
+        southEastPanel.add(p2Color3);
+        southCenterPanel.add(showRulesButton);
+        southCenterPanel.add(undoButton);
 
 
 
 
 
 
+
+        setSize(600,600);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+    }
+
+    public static void main(String[] args) {
+        GameGUI gg = new GameGUI();
     }
 
     public void refreshGameGrid (){
